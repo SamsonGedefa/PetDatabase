84277820f6f0d01ecc3324e017ce8737b8c8c7f8
Samson Gedefa <samsongedefa@gmail.com>
2022-05-18 19:39:23 -0500

    added search function

--- src/Pet.java
+++ src/Pet.java
@@ -83,7 +83,7 @@ public class Pet {
         int option;
         do {
             System.out.println("What would like to do?\n" + "\t1) View all pets\n" + "\t2) Add more pets\n"
-                    + "\t3) Exit program");
+                    + "\t3) Search pets by name\n" + "\t4) Search pets by age\n" + "\t5) Exit program\n");
             System.out.print("Your choice: ");
             option = scan.nextInt();
             scan.nextLine();
@@ -94,8 +94,14 @@ public class Pet {
                 case 2:
                     addMorePets(scan);
                     break;
-
                 case 3:
+                    searchByPetName(scan);
+                    break;
+                case 4:
+                    searchByPetAge(scan);
+                    break;
+
+                case 5:
                     System.out.println("Goodbye!");
                     break;
 
@@ -104,13 +110,13 @@ public class Pet {
                     break;
             }
 
-        } while (option != 3);
+        } while (option != 5);
 
         scan.close();
     }
 
     /**
-     * View all pets.
+     * Display all pets in the database
      */
     private static void viewAllPets() {
 
@@ -129,9 +135,9 @@ public class Pet {
     }
 
     /**
-     * Adds the more pets.
+     * Adds more pets to database.
      *
-     * @param scan the scan
+     * @param scan
      */
     private static void addMorePets(Scanner scan) {
         int count = 0;
@@ -141,7 +147,6 @@ public class Pet {
             System.out.print("add pet (name, age): ");
             petString = scan.nextLine();
             if (petString.equalsIgnoreCase("done")) {
-
                 break;
             }
             String name = petString.split("\\s+")[0];
@@ -155,4 +160,58 @@ public class Pet {
     }
 
 
+    /**
+     * Search by pet name
+     *
+     * @param scan
+     */
+    private static void searchByPetName(Scanner scan) {
+
+        System.out.print("Enter name to search: ");
+        String name = scan.nextLine();
+
+        System.out.println("+---------------------------------------+");
+        System.out.printf("|%5s%5s%10s%10s%5s%5s\n", "ID", "|", "NAME", "|", "AGE", "|");
+        System.out.println("+---------------------------------------+");
+        int i = 0;
+        for (Pet pet : pets) {
+
+            if (pet.getName().equalsIgnoreCase(name)) {
+                System.out.printf("|%5d%5s", i, pet.toString());
+                i++;
+            }
+        }
+        System.out.println("+---------------------------------------+");
+        System.out.println((i) + "rows in set.");
+
+    }
+
+    /**
+     * Search by pet age
+     *
+     * @param scan
+     */
+    private static void searchByPetAge(Scanner scan) {
+
+        System.out.print("Enter age to search: ");
+        int age = scan.nextInt();
+        scan.nextLine();
+        System.out.println("+---------------------------------------+");
+        System.out.printf("|%5s%5s%10s%10s%5s%5s\n", "ID", "|", "NAME", "|", "AGE", "|");
+        System.out.println("+---------------------------------------+");
+        int i = 0;
+        for (Pet pet : pets) {
+
+            if (pet.getAge() == age) {
+
+                System.out.printf("|%5d%5s", i, pet);
+                i++;
+            }
+        }
+        System.out.println("+---------------------------------------+");
+        System.out.println((i) + "rows in set.");
+
+    }
+
 }
+
